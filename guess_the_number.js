7c52ce41e78a3687f1bb8505eae300779cf8273a
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-05-06 14:48:13 -0400

    added comments

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,13 +1,13 @@
 //guess a number between 1 and 100
 
 var correctNum = Math.floor((Math.random()* 100) + 1);
-
 var userGuess = Number(prompt("Guess a number between 1 and 100"));
 var youWon = false;
-for(var i = 0; i < 7; i++) {
- if (userGuess > correctNum) {
+//loop a certain number of guesses
+for(var i = 0; i <= 7; i++) {
+ if (userGuess > correctNum) { //evaluates guess
   console.log(userGuess + " is too high");
-  userGuess = Number(prompt("Guess Again..."));
+  userGuess = Number(prompt("Guess Again...")); //resets guess
 } else if (userGuess < correctNum) {
   console.log(userGuess + " is too low");
   userGuess = Number(prompt("Try Again"));
