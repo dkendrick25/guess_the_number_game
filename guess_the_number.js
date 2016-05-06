80498f43409136ac1ceee8bd5566883476cd571d
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-05-06 15:09:24 -0400

    fixed to evaluate the YOU LOSE statement

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,16 +1,14 @@
 //guess a number between 1 and 100
 
 var correctNum = Math.floor((Math.random()* 100) + 1);
-var userGuess = Number(prompt("Guess a number between 1 and 100"));
 var youWon = false;
 //loop a certain number of guesses
 for(var i = 0; i <= 7; i++) {
+  var userGuess = Number(prompt("Guess a number between 1 and 100"));
  if (userGuess > correctNum) { //evaluates guess
   console.log(userGuess + " is too high");
-  userGuess = Number(prompt("Guess Again...")); //resets guess
 } else if (userGuess < correctNum) {
   console.log(userGuess + " is too low");
-  userGuess = Number(prompt("Try Again"));
 } else if (userGuess === correctNum) {
   console.log("YOU WIN!");
   youWon = true;
